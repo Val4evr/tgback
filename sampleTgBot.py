d44f63ec27bb790e5457089a1936206a1550acc7
Val4evr <valeriyproklov501@gmail.com>
2025-11-18 18:52:14 +0100

    first

--- sampleTgBot.py
+++ sampleTgBot.py
@@ -1,37 +1,40 @@
 import telebot
+from random import choice
 
-bot = telebot.TeleBot('7024503878:AAF-vE9cLNVam9NzrhXmQPwqZR5yAxBtrn4')
+bot = telebot.TeleBot('8514119529:AAH4Ew10zbCpRUTAP8j7RWAfu_D0ljhpjQU')
 print('hey')
 
-#start command
+# start command
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     user = message.chat.id
-    bot.send_message(user,'Hello \n give . ')
+    bot.send_message(user, 'Hello \n give . ')
 
-#main code
+# main code
+
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     user = message.chat.id
-    
+
     if message.text == "help":
-        bot.send_message(user,'instruction ... ')
+        bot.send_message(user, 'instruction ... ')
 
     elif message.text == '🤍':
         bot.send_message(user, 'you too ⛄')
 
-    elif message.text == 'bye':
+    elif message.text == 'country':
+
+    elif message.text == '':
         bot.send_message(user, 'well')
         bot.send_message(user, 'see you')
     else:
         bot.send_message(message.chat.id, '¯\_(ツ)_/¯ ')
 
-bot.infinity_polling()
 
-   
+bot.infinity_polling()
 
 
 # bot.send_photo(message.chat.id, 'https://drive.google.com/file/d/1DuCZu_U4R2xA0ZNzr6ln2EczkWKHyaiE/view?usp=sharing')
-
-
-
